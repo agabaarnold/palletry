53db9f2d21d1ef5a86abf9311a070aba654c62ee
Agaba Arnold <agabaarnold23@gmail.com>
2026-08-18 15:16:33 +0300

    chore: Add database relations to drizzle initialization

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,7 +1,11 @@
 import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
 import { env } from "#/env.ts";
+import { authRelations, mainRelations } from "./relations";
 
 const pool = new Pool({ connectionString: env.DATABASE_URL });
 
-export const db = drizzle({ client: pool });
+export const db = drizzle({
+	client: pool,
+	relations: { ...mainRelations, ...authRelations },
+});
