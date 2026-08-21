b85af72e13335c4ed60d1db9ab0f9edb9f3c127d
Agaba Arnold <agabaarnold23@gmail.com>
2026-08-21 11:13:47 +0300

    feat: Implement route for application entry with session validation

---
+++ src/routes/_app/route.tsx
@@ -0,0 +1,18 @@
+import { createFileRoute, redirect } from "@tanstack/react-router";
+import { getSession } from "#/features/auth/functions/index.ts";
+
+export const Route = createFileRoute("/_app")({
+	beforeLoad: async ({ location }) => {
+		const session = await getSession();
+		if (!session) {
+			throw redirect({ to: "/sign-in", search: { redirect: location.href } });
+		}
+
+		return { session };
+	},
+	component: AppLayout,
+});
+
+function AppLayout() {
+	return <div>Hello "/_app"!</div>;
+}
