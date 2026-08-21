389984c756d16d85cb6c5782babb7a8995055b59
Agaba Arnold <agabaarnold23@gmail.com>
2026-08-21 11:11:35 +0300

    feat: Add FormCheckbox component for enhanced form functionality

---
+++ src/components/form/form-checkbox.tsx
@@ -0,0 +1,26 @@
+import { useFieldContext } from "#/hooks/forms/use-form-context.ts";
+import { Checkbox } from "../ui/checkbox";
+import { Field, FieldLabel } from "../ui/field";
+
+interface FormCheckboxProps {
+	label: string;
+}
+
+const FormCheckbox = ({ label }: FormCheckboxProps) => {
+	const field = useFieldContext<boolean>();
+
+	return (
+		<Field orientation="horizontal">
+			<Checkbox
+				checked={field.state.value}
+				id={field.name}
+				name={field.name}
+				onCheckedChange={(checked) => field.handleChange(checked)}
+			/>
+            
+			<FieldLabel htmlFor={field.name}>{label}</FieldLabel>
+		</Field>
+	);
+};
+
+export default FormCheckbox;
