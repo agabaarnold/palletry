4bdc8c5c24d6de84ae59f8aca9e43820e3cf9582
Agaba Arnold <agabaarnold23@gmail.com>
2026-08-21 13:57:47 +0300

    feat: Add environment variables for authentication and SMTP configuration

--- src/env.ts
+++ src/env.ts
@@ -5,6 +5,14 @@ export const env = createEnv({
 	emptyStringAsUndefined: true,
 	runtimeEnv: process.env,
 	server: {
+		BETTER_AUTH_SECRET: z.string(),
+		BETTER_AUTH_URL: z.url(),
 		DATABASE_URL: z.url(),
+		NODE_ENV: z.enum(["development", "production"]).default("production"),
+		SMTP_FROM: z.string().min(1),
+		SMTP_HOST: z.string(),
+		SMTP_PASS: z.string(),
+		SMTP_PORT: z.coerce.number(),
+		SMTP_USER: z.string(),
 	},
 });
